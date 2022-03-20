0a5d758a7f1ab3f694065133a19e170e59771da1
Antwaun Johnson <36043929+antwaunjohnson@users.noreply.github.com>
2022-03-19 22:43:49 -0700

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace MyPersonalBlog.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
