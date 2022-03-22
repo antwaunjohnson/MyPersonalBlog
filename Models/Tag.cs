c92f4c724249afca1ccd7ce5e6d286710a704779
Antwaun Johnson <36043929+antwaunjohnson@users.noreply.github.com>
2022-03-21 21:32:29 -0700

    Created model for tags

---
+++ Models/Tag.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+
+namespace MyPersonalBlog.Models;
+
+public class Tag
+{
+    public int TagId { get; set; }
+    public int PostId { get; set; }
+    public string? AuthorId { get; set; }
+
+    [Required]
+    [StringLength(25, ErrorMessage = "The {0} must be at least {2} and no more than {1} characters long", MinimumLength = 2)]
+    public string? Text { get; set; }
+
+    public virtual Post? Post { get; set; }
+    public virtual IdentityUser? Author { get; set; }
+}
