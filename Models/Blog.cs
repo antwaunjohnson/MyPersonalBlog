6ac426cb1e91af93f4446342e55f09b947d3e1ed
Antwaun Johnson <36043929+antwaunjohnson@users.noreply.github.com>
2022-03-21 19:03:27 -0700

    created initial model for Blog

---
+++ Models/Blog.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MyPersonalBlog.Models;
+
+public class Blog
+{
+    public int BlogId { get; set; }
+    public string? AuthorId { get; set; }
+    [Required]
+    [StringLength(150, ErrorMessage = "The {0} must be at least {2} and have a maximum of {1} characters", MinimumLength = 5)]
+    public string? Name { get; set; }
+
+    [StringLength(500, ErrorMessage = "The {0} must be at least {2} and have a maximum of {1} characters", MinimumLength = 5)]
+    public string? Description { get; set; }
+    [DataType(DataType.Date)]
+    [Display(Name = "Created Date")]
+    public DateTime? Created { get; set; }
+
+    [DataType(DataType.Date)]
+    [Display(Name = "Updated Date")]
+    public DateTime? Updated { get; set; }
+
+    [Display(Name = "Blog Image")]
+    public byte[]? ImageData { get; set; }
+
+    [Display(Name = "Image Type")]
+    public string? ContentType { get; set; }
+
+    [NotMapped]
+    public IFormFile? Image { get; set; }
+}
