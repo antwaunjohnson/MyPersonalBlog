a274451218dae9110d6366154337b61b3afd013f
Antwaun Johnson <36043929+antwaunjohnson@users.noreply.github.com>
2022-04-28 20:20:47 -0700

    update application to allow users to upload an image during registration

--- Areas/Identity/Pages/Account/Register.cshtml.cs
+++ Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -31,13 +31,15 @@ namespace MyPersonalBlog.Areas.Identity.Pages.Account
         private readonly IUserEmailStore<BlogUser> _emailStore;
         private readonly ILogger<RegisterModel> _logger;
         private readonly IBlogEmailSender _emailSender;
+        private readonly IImageService _imageService;
+        private readonly IConfiguration _configuration;
 
         public RegisterModel(
             UserManager<BlogUser> userManager,
             IUserStore<BlogUser> userStore,
             SignInManager<BlogUser> signInManager,
             ILogger<RegisterModel> logger,
-            IBlogEmailSender emailSender)
+            IBlogEmailSender emailSender, IImageService imageService, IConfiguration configuration)
         {
             _userManager = userManager;
             _userStore = userStore;
@@ -45,6 +47,8 @@ namespace MyPersonalBlog.Areas.Identity.Pages.Account
             _signInManager = signInManager;
             _logger = logger;
             _emailSender = emailSender;
+            _imageService = imageService;
+            _configuration = configuration;
         }
 
         /// <summary>
@@ -72,6 +76,9 @@ namespace MyPersonalBlog.Areas.Identity.Pages.Account
         /// </summary>
         public class InputModel
         {
+            [Display(Name = "Custom Image")]
+            public IFormFile ImageFile { get; set; }
+
             [Required]
             [StringLength(50, ErrorMessage = "The {0} needs to be at least {2} and no more than {1} characters long", MinimumLength = 3)]
             [Display(Name = "First Name")]
@@ -135,7 +142,12 @@ namespace MyPersonalBlog.Areas.Identity.Pages.Account
                     LastName = Input.LastName,
                     DisplayName = Input.DisplayName,
                     Email = Input.Email,
-                    UserName = Input.Email
+                    UserName = Input.Email,
+                    ImageData = await (_imageService.EncodeImageAsync(Input.ImageFile)) ??
+                                await _imageService.EncodeImageAsync(_configuration["DefaultUserImage"]),
+                    ContentType = Input.ImageFile is null ? 
+                                    Path.GetExtension(_configuration["DefaultUserImage"]) :
+                                    _imageService.ContentType(Input.ImageFile)
                 };
 
                 //await _userStore.SetUserNameAsync(user, Input.Email, CancellationToken.None);
